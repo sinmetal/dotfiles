19b8d6d6f2f00792e16cb030e07dcba7ab9e5152
Shingo Ishimura <metal.tie@gmail.com>
2017-06-19 20:14:59 +0900

    kerberos移行に伴いメンテナンスを実施

--- brew.sh
+++ brew.sh
@@ -5,10 +5,9 @@ export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 brew update
 brew upgrade
 
-brew install caskroom/cask/brew-cask
-brew tap caskroom/versions
+brew tap caskroom/cask
 
-# brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup
+brew cask install java # javaが必要なものが多いので最初にインストールする
 
 brew install wget
 brew install tree
@@ -58,25 +57,17 @@ brew cask install alfred
 brew cask install google-hangouts
 brew cask install skype
 brew cask install dropbox
-brew cask install kobito
 brew cask install marsedit
 brew cask install skitch
 # 課金があるので、AppStoreから入れる
 # brew cask install dash
-brew cask install sublime-text
-brew cask install sophos-anti-virus-home-edition
 brew cask install xtrafinder
 brew cask install iterm2
-brew cask install adobe-reader
 brew cask install libreoffice
 brew cask install macvim
 brew cask install intellij-idea-ce
 brew cask install atom
+brew cask install slack
 
-brew cask install java
-brew cask install java7
-
-brew cask alfred link 
-
-brew link openssl --force
-brew link git --force
+brew unlink openssl && brew link openssl
+brew unlink git && brew link git
